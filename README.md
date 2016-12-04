7eec26c2baa2094870c85e3713ea9820b6656516
rhiokim <rhio.kim@gmail.com>
2016-12-04 21:39:09 +0900

    chore: add module for docker shell cmd

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 ### How to run on local
 
 ```
-$ PORT=7777 SECRET="YOUR_SECRET_KEY" npm start
+$ PORT=7777 \
+  SECRET="WEBHOOK-SECRET-KEY" \
+  REGISTRY_HOST=registry \
+  REGISTRY_PORT=5000 \
+  npm start
 ```
+
+### Issue with private docker registry
+
+- http://stackoverflow.com/questions/38695515/can-not-pull-push-images-after-update-docker-to-1-12
+- https://forums.docker.com/t/how-to-run-a-insecure-registry/9692/15
+- http://stackoverflow.com/questions/37619277/docker-mac-beta-using-insecure-private-registry
--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const path = require('path')
 const rimraf = require('rimraf')
 const createHandler = require('github-webhook-handler')
 const dockerBuild = require('docker-build')
+const dockerPush = require('docker-push')
 
 const pull = require('./lib/pull')
-const port = process.env.PORT || 7777
-const secret = process.env.SECRET || process.argv[2] || ''
+const PORT = process.env.PORT || 7777
+const SECRET = process.env.SECRET || process.argv[2] || ''
+const REGISTRY_HOST = process.env.REGISTRY_HOST || 'localhost'
+const REGISTRY_PORT = process.env.REGISTRY_PORT || 5000
 
-const handler = createHandler({ path: '/webhook', secret: secret })
+const handler = createHandler({ path: '/webhook', secret: SECRET })
 
 http.createServer((req, res) => {
   handler(req, res, err => {
     res.statusCode = 404
     res.end('no such location')
   })
-}).listen(port)
+}).listen(PORT)
 
 handler.on('error', err => {
   console.error('Error:', err.message)
@@ -24,10 +27,16 @@ handler.on('error', err => {
 handler.on('push', event => {
   const {repository, head_commit} = event.payload
   const target = path.join(process.cwd(), 'tmp', repository.full_name, head_commit.id)
+
   rimraf.sync(target)
+
   pull(repository.ssh_url, target)
     .then(() => {
-      dockerBuild('pipe', target)
+      dockerBuild(repository.full_name, target, () => {
+        dockerPush(repository.full_name, `${REGISTRY_HOST}:${REGISTRY_PORT}/${repository.full_name}`, () => {
+          console.log('done')
+        })
+      })
     })
     .catch(err => {
       console.log(err)
