d51c4b4d45aa8cd1d5d891678e1d0d81c5e361c4
rhiokim <rhio.kim@gmail.com>
2016-12-13 00:38:23 +0900

    fix: CORS issue

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const http = require('http')
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
 const displayRoutes = require('express-routemap')
 
 const registry = require('./routes/registry')
 const webhook = require('./routes/webhook')
 
-const PORT = process.env.PORT || 7777
+const PORT = process.env.PORT || 8082
 
 const app = express()
+app.use(cors())
 app.use(express.static(path.join(__dirname, '..', 'www')))
 
 app.use('/registry', registry)
