5bbc567bf935a30fc1cb79849499133f35d9f27a
rhiokim <rhio.kim@gmail.com>
2017-01-23 15:20:26 +0900

    feat: basically root api return default message

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const path = require('path')
 const displayRoutes = require('express-routemap')
 
+const pkg = require('./package.json')
 const registry = require('./routes/registry')
 const webhook = require('./routes/webhook')
 const sshkey = require('./routes/sshkey')
@@ -14,10 +15,16 @@ const app = express()
 app.use(cors())
 app.use(express.static(path.join(__dirname, '..', 'www')))
 
+app.get('/', (req, res) => {
+  res.send(`${pkg.name} v${pkg.version}`)
+})
+
 app.use('/registry', registry)
 app.use('/webhook', webhook)
 app.use('/sshkey', sshkey)
 
 http.createServer(app).listen(PORT, () => {
   displayRoutes(app)
+
+  console.log('start server localhost: %s', PORT)
 })
